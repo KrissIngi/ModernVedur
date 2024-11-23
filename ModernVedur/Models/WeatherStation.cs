2c1ae0c9840785e165b061d223c6fb0a2570291c
Krissi <kristjaningi07@gmail.com>
2024-11-23 13:20:01 +0000

    Adding Models and weather station modal for data gathering.

---
+++ ModernVedur/Models/WeatherStation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModernVedur.Models
+{
+    public class WeatherStation
+    {
+        public string? name { get; set; }
+        public DateTime? date { get; set; }
+        public int temperature {  get; set; }
+        public int windSpeed { get; set; }
+        public int precipation { get; set; }
+    }
+}
