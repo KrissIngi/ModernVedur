171a06c1eab952cc421b1f3d1e0e6d2da59885cf
Krissi <kristjaningi07@gmail.com>
2024-11-24 15:42:22 +0000

    Moving JS functions together and calling them with parameters, updating Weather.cshtml with the new changes.

--- ModernVedur/wwwroot/js/site.js
+++ ModernVedur/wwwroot/js/site.js
@@ -1,26 +1,36 @@
 ﻿// Custom JS code for Weather page.
 // Function used to show the map when clicking 'Sýna kort' used with transition for better UI experience.
-function showMap() {
-    var mapDiv = document.getElementById('MapColumn');
+function show(elementID, buttonName) {
+    var mapDiv = document.getElementById(elementID);
     mapDiv.classList.add('show');
-    document.getElementById('ShowButton').style.display = 'none';
-    document.getElementById('HideButton').style.display = '';
+    document.getElementById('show' + buttonName).style.display = 'none';
+    document.getElementById('hide' + buttonName).style.display = '';
+
+    //var mapDiv = document.getElementById('MapColumn');
+    //mapDiv.classList.add('show');
+    //document.getElementById('ShowButton').style.display = 'none';
+    //document.getElementById('HideButton').style.display = '';
 }
 
 // Function used to hide the map when clicking 'Fela kort' used with transition for better UI experience.
-function hideMap() {
-    var mapDiv = document.getElementById('MapColumn');
+function hide(elementID, buttonName) {
+    var mapDiv = document.getElementById(elementID);
     mapDiv.classList.remove('show');
-    document.getElementById('ShowButton').style.display = '';
-    document.getElementById('HideButton').style.display = 'none';
+    document.getElementById('show' + buttonName).style.display = '';
+    document.getElementById('hide' + buttonName).style.display = 'none';  
+
+    //var mapDiv = document.getElementById('MapColumn');
+    //mapDiv.classList.remove('show');
+    //document.getElementById('ShowButton').style.display = '';
+    //document.getElementById('HideButton').style.display = 'none';
 }
 
 // Function used to show the future weather when clicking 'Spá næstu daga' used with transition for better UI experience.
 function showFutureWeather() {
     var mapDiv = document.getElementById('FutureWeatherColumn');
     mapDiv.classList.add('show');
-    document.getElementById('showFutureButton').style.display = 'none';
-    document.getElementById('hideFutureButton').style.display = '';
+    document.getElementById('show' + FutureButton).style.display = 'none';
+    document.getElementById('hide' + FutureButton).style.display = '';
 }
 
 // Function used to hide the map when clicking 'Fela kort' used with transition for better UI experience.
